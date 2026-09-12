3564509899d5c6e856158665bc5b44dd41d4bc70
Bieseche2 <biesechesayajin@outlook.com>
2026-09-11 22:43:44 -0300

    fix: sobe Kotlin plugin pra 2.4.0 (bate com o stdlib puxado pelo Coil, causava erro interno do compilador)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,5 @@ plugins {
     // AGP 8.13.0: suporta compileSdk até API 36.1, exige Gradle 8.13+ e JDK 17
     id("com.android.application") version "8.13.0" apply false
     // Kotlin 2.1.0: versão estável documentada, compatível com AGP 8.13
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.4.0" apply false
 }
